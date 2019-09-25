b53e1ec8ad8060e2af4c40b7a716edcfe229cdd0
Mohit Singh <mohitsingh1997@gmail.com>
2019-09-25 12:12:30 +0530

    Added sign-in capabilities

--- src/App.js
+++ src/App.js
@@ -6,20 +6,17 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/signin-signup/signin-signup.component';
 
-const HatsPage = () => (
-	<div>
-		<h1>Hats Page</h1>
-	</div>
-);
 
 function App() {
   return (
     <div>
     	<Header />
     	<Switch>
-      		<Route exact path='/' component={HomePage}/>
-      		<Route path='/shop' component={ShopPage}/>
+      		<Route exact path='/' component={HomePage} />
+      		<Route path='/shop' component={ShopPage} />
+      		<Route path='/signin' component={SignInAndSignUpPage} />
       	</Switch>
     </div>
   );
